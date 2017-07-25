e8b014cca88e153f3df231bedce85fbe53d967b5
Tzu-Wei Huang <huang.dexter@gmail.com>
2017-07-25 17:41:10 +0800

    clean up

--- demo_embedding.py
+++ demo_embedding.py
@@ -2,7 +2,7 @@ from tensorboard.embedding import add_embedding
 import keyword
 import torch
 meta = []
-for i in range(10):
+while len(meta)<100:
     meta = meta+keyword.kwlist
 meta = meta[:100]
 
@@ -13,4 +13,8 @@ label_img = torch.rand(100, 3, 10, 32)
 for i in range(100):
     label_img[i]*=i/100
 
-add_embedding(torch.randn(100, 5), save_path='embedding', metadata=meta, label_img=label_img)
+add_embedding(torch.randn(100, 5), save_path='embedding1', metadata=meta, label_img=label_img)
+add_embedding(torch.randn(100, 5), save_path='embedding2', label_img=label_img)
+add_embedding(torch.randn(100, 5), save_path='embedding3', metadata=meta)
+
+#tensorboard --logdir embedding1
--- tensorboard/embedding.py
+++ tensorboard/embedding.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 import os
 
-def make_tsv(metadata, save_path=None):
+def make_tsv(metadata, save_path):
     metadata = [str(x) for x in metadata]
     with open(os.path.join(save_path, 'metadata.tsv'), 'w') as f:
         for x in metadata:
@@ -10,7 +10,7 @@ def make_tsv(metadata, save_path=None):
 
 
 # https://github.com/tensorflow/tensorboard/issues/44 image label will be squared
-def make_sprite(label_img, save_path=None):
+def make_sprite(label_img, save_path):
     import math
     nrow = math.floor(math.sqrt(label_img.size(0)))
     xx = torchvision.utils.make_grid(torch.Tensor(1,3,32,32), padding=0)
@@ -21,11 +21,12 @@ def make_sprite(label_img, save_path=None):
     else:
         torchvision.utils.save_image(label_img, os.path.join(save_path, 'sprite.png'), nrow=nrow, padding=0)
 
-def make_pbtxt(label_img=None, save_path=None):
+def make_pbtxt(save_path, metadata, label_img):
     with open(os.path.join(save_path, 'projector_config.pbtxt'), 'w') as f:
         f.write('embeddings {\n')
         f.write('tensor_name: "embedding:0"\n')
-        f.write('metadata_path: "metadata.tsv"\n')
+        if metadata is not None:
+            f.write('metadata_path: "metadata.tsv"\n')
         if label_img is not None:
             f.write('sprite {\n')
             f.write('image_path: "sprite.png"\n')
@@ -36,15 +37,14 @@ def make_pbtxt(label_img=None, save_path=None):
 
 
 '''
-save_path: self-explained.
 mat: torch tensor. mat.size(0) is the number of data. mat.size(1) is the cardinality of feature dimensions
+save_path: self-explained.
 metadata: a list of {int, string} of length equals mat.size(0)
 label_img: 4D torch tensor. label_img.size(0) equals mat.size(0). 
 
 '''
 
-def add_embedding(mat, save_path=None, metadata=None, label_img=None):
-    assert save_path is not None, 'please specify path to log files'
+def add_embedding(mat, save_path, metadata=None, label_img=None):
     os.makedirs(save_path, exist_ok=True)
     if metadata is not None:
         assert mat.size(0)==len(metadata), '#labels should equal with #data points'
@@ -53,11 +53,12 @@ def add_embedding(mat, save_path=None, metadata=None, label_img=None):
         assert mat.size(0)==label_img.size(0), '#images should equal with #data points'
         make_sprite(label_img, save_path)
     import tensorflow as tf
+    tf.reset_default_graph()
     emb = tf.Variable(mat.tolist(), name="embedding")
     with tf.Session() as sess:
         sess.run(emb.initializer)
         saver = tf.train.Saver()
         saver.save(sess, save_path=os.path.join(save_path, 'model.ckpt'), global_step=None, write_meta_graph=False)
-    make_pbtxt(label_img, save_path=save_path)
+    make_pbtxt(save_path, metadata, label_img)
 
 
