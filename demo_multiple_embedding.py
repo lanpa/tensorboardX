d06294682a14cc9ea74e02e626e9e8eec89eee9f
Binesh Bannerjee <binesh_binesh@hotmail.com>
2018-03-15 10:18:23 -0400

    xrange changed to range

--- demo_multiple_embedding.py
+++ demo_multiple_embedding.py
@@ -10,13 +10,13 @@ from tensorboardX import SummaryWriter
 
 def main():
     degrees = torch.arange(3600).resize_(3600, 1) * math.pi / 180.0
-    labels = ["%d" % (i) for i in xrange(0, 3600)]
+    labels = ["%d" % (i) for i in range(0, 3600)]
 
     with SummaryWriter() as writer:
         # Maybe make a bunch of data that's always shifted in some
         # way, and that will be hard for PCA to turn into a sphere?
 
-        for epoch in xrange(0, 16):
+        for epoch in range(0, 16):
             shift = epoch * 2 * math.pi / 16.0
             mat = torch.cat([ \
                 torch.sin(shift + degrees * 2 * math.pi / 180.0), \
