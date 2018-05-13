3a24b727d0f78d43e3e50ffc7c69ff6fa8b57331
Fartash Faghri <fartash.faghri@gmail.com>
2018-05-13 16:14:43 -0400

    More detailed docstring for kwargs in SummaryWriter

--- tensorboardX/writer.py
+++ tensorboardX/writer.py
@@ -231,7 +231,9 @@ class SummaryWriter(object):
             log_dir (string): save location, default is: runs/**CURRENT_DATETIME_HOSTNAME**, which changes after each
               run. Use hierarchical folder structure to compare between runs easily. e.g. 'runs/exp1', 'runs/exp2'
             comment (string): comment that appends to the default log_dir
-            kwargs: extra keyword arguments for FileWriter
+            kwargs: extra keyword arguments for FileWriter (i.e. 'graph',
+              'max_queue', 'flush_secs', 'graph_def', and filename_suffix).
+              Specifically, 'flush_secs' controls how often to flush pending events.
         """
         if not log_dir:
             import socket
