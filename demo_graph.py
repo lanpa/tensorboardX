d3b2e7dd4709b82ad4087e5b136a3b5789f76827
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-07-04 22:48:55 +0800

    revert changed file permition
    merge demo

--- demo_graph.py
+++ demo_graph.py
@@ -129,7 +129,48 @@ with SummaryWriter(comment='basicblock') as w:
     model = BasicBlock(3,3)
     w.add_graph(model, (dummy_input, ))#, verbose=True)
 
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+n_letters = 100
+n_hidden = 128
+n_categories = 10
+rnn = RNN(n_letters, n_hidden, n_categories)
+cat = torch.Tensor(1, n_categories)
+dummy_input = torch.Tensor(1, n_letters)
+hidden = torch.Tensor(1, n_hidden)
+
+
+out, hidden = rnn(cat, dummy_input, hidden)
+with SummaryWriter(comment='RNN') as w:
+    w.add_graph(rnn, (cat, dummy_input, hidden), verbose=False)
+
+
 import pytest
+print('expect error here:')
 with pytest.raises(Exception) as e_info:
     dummy_input = torch.rand(1, 1, 224, 224)
     with SummaryWriter(comment='basicblock_error') as w:
