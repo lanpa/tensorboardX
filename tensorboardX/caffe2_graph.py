6c98970cc4c5466f936995e68c5a38e7913e21eb
Michael Li <ml@instagram.com>
2018-07-05 11:28:27 -0700

    Handle model_helper.ModelHelper
    Previously was only handling cnn.CNNModelHelper.

--- tensorboardX/caffe2_graph.py
+++ tensorboardX/caffe2_graph.py
@@ -10,7 +10,7 @@ import six
 from builtins import bytes
 
 from caffe2.proto import caffe2_pb2
-from caffe2.python import core
+from caffe2.python import core, workspace
 
 from .src.graph_pb2 import GraphDef
 from .src.node_def_pb2 import NodeDef
