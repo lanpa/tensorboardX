ec20d46e609db88689feb32be2ae9b3212cd3693
Michael Li <ml@instagram.com>
2018-07-10 09:53:20 -0700

    Change StringIO to BytesIO for Travis CI

--- demo_caffe2.py
+++ demo_caffe2.py
@@ -28,11 +28,11 @@ print("Necessities imported!")
 def DownloadResource(url, path):
     '''Downloads resources from s3 by url and unzips them to the provided path'''
     import requests
-    from six import StringIO
+    from six import BytesIO
     import zipfile
     print("Downloading... {} to {}".format(url, path))
     r = requests.get(url, stream=True)
-    z = zipfile.ZipFile(StringIO(r.content))
+    z = zipfile.ZipFile(BytesIO(r.content))
     z.extractall(path)
     print("Completed download and extraction.")
 
