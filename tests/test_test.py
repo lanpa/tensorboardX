2d93bec67bb46e985e459d57b6321873aae08a80
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-08-08 22:01:33 +0800

    reduce clutter

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,8 +1,10 @@
 def test_test():
     import demo
-    import demo_graph
-    import demo_embedding
-
+    from examples import demo_graph
+    from examples import demo_embedding
+    from examples import demo_multiple_embedding
+    from examples import demo_purge
+    from examples import matplotlib_demo
     import subprocess
     subprocess.check_output(['flake8','tensorboardX'])
     
