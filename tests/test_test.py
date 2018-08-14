ae064787e3b080ce9156d7c2e50be5cba0d85e43
Michael Li <ml@instagram.com>
2018-08-14 14:33:21 -0700

    Move demo.py to examples folder
    Also rename matplotlib_demo to demo_matplotlib for consistency with the rest of the demos in the /examples folder.

--- tests/test_test.py
+++ tests/test_test.py
@@ -5,7 +5,6 @@ def test_test():
     from examples import demo_custom_scalars
     from examples import demo_multiple_embedding
     from examples import demo_purge
-    from examples import matplotlib_demo
+    from examples import demo_matplotlib
     import subprocess
-    subprocess.check_output(['flake8','tensorboardX'])
-    
+    subprocess.check_output(['flake8', 'tensorboardX'])
