lanpa/tensorboardX

commit b5ac736ff2e93dac2cd283610e309b38dd20d58d

Dynamically generate protobuf python interfaces (#237)
* Dynamically generate Python *_pb2.py output

Adds to setup.py so that the shell script to compile the .proto files with protoc is run post-install.

Rename the tensorboardX/src directory -> tensorboardX/proto as this makes more sense. Adjust all usages of ..src, .src, tensorboardX.src to ..proto, .proto, tensorboardX.proto.

* Move demo.py to examples folder

Also rename matplotlib_demo to demo_matplotlib for consistency with the rest of the demos in the /examples folder.

* Rename graph_onnx -> onnx_graph

For consistency with pytorch_graph.py and caffe2_graph.py.

* Fix tests

compile.sh
@@ -1,1 +1,12 @@
-protoc tensorboardX/src/*.proto --python_out=.
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Delete all existing Python protobuf (*_pb2.py) output
+rm -rf tensorboardX/proto/*pb2*.py
+
+# Regenerate
+protoc tensorboardX/proto/*.proto --python_out=.
+
+echo "Done generating tensorboardX/proto/*pb2*.py"

setup.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+import subprocess
 
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        # Dynamically compile protos
+        subprocess.call(['./compile.sh'])
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # Dynamically compile protos
+        subprocess.call(['./compile.sh'])
+        install.run(self)
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
@@ -22,7 +41,7 @@ setup(
     name='tensorboardX',
     version='1.4',
     description='TensorBoardX lets you watch Tensors Flow without Tensorflow',
-    long_description= history,
+    long_description=history,
     author='Tzu-Wei Huang',
     author_email='huang.dexter@gmail.com',
     url='https://github.com/lanpa/tensorboardX',
@@ -37,12 +56,17 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',        
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+    scripts=['compile.sh'],
     test_suite='tests',
     tests_require=test_requirements
 )

.flake8
@@ -1,4 +1,4 @@
 [flake8]
 max-line-length = 120
 ignore = E305,E402,E721,E741,F401,F403,F405,F821,F841,F999
-exclude = tensorboardX/src
+exclude = tensorboardX/proto

.gitignore
@@ -1,3 +1,4 @@
+tensorboardX/proto/*_pb2.py
 proto_src/
 __pycache__
 docs/_*

MANIFEST.in
@@ -1,2 +1,3 @@
 include HISTORY.rst
 include LICENSE
+recursive-include tensorboardX/proto *

tensorboardX/beholder/beholder.py
@@ -15,10 +15,10 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from ..src.summary_pb2 import Summary
-from ..src.summary_pb2 import SummaryMetadata
-from ..src.tensor_pb2 import TensorProto
-from ..src.tensor_shape_pb2 import TensorShapeProto
+from ..proto.summary_pb2 import Summary
+from ..proto.summary_pb2 import SummaryMetadata
+from ..proto.tensor_pb2 import TensorProto
+from ..proto.tensor_shape_pb2 import TensorShapeProto
 
 import os
 import time

commit ba933946ffcb94f26f748e789d845e899ff9715f

install protoc to fix documentation issue caused by #237

compile.sh
@@ -6,7 +6,17 @@ set -e
 # Delete all existing Python protobuf (*_pb2.py) output
 rm -rf tensorboardX/proto/*pb2*.py
 
+
+# Download protoc. Make sure we are using same version of protoc
+protoc="protoc-3.6.1-linux-x86_64.zip"
+if [ ! -f $protoc ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/$protoc
+fi
+rm -rf protoc
+python -c "import zipfile; zipfile.ZipFile('protoc-3.6.1-linux-x86_64.zip','r').extractall('protoc')"
+chmod +x protoc/bin/protoc
+
 # Regenerate
-protoc tensorboardX/proto/*.proto --python_out=.
+protoc/bin/protoc tensorboardX/proto/*.proto --python_out=.
 
 echo "Done generating tensorboardX/proto/*pb2*.py"

setup.py
@@ -12,7 +12,8 @@ class PostDevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
         # Dynamically compile protos
-        subprocess.call(['./compile.sh'])
+        res = subprocess.call(['./compile.sh'])
+        assert res == 0, 'cannot compile protobuf'
         develop.run(self)
 
 
@@ -20,7 +21,8 @@ class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
         # Dynamically compile protos
-        subprocess.call(['./compile.sh'])
+        res = subprocess.call(['./compile.sh'])
+        assert res == 0, 'cannot compile protobuf'
         install.run(self)
 
 with open('HISTORY.rst') as history_file: