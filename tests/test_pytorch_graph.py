8b199e43ad899eab1d5787a43a0db4d2fc60615f
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-11-27 08:32:34 +0800

    use unittest for #287

--- tests/test_pytorch_graph.py
+++ tests/test_pytorch_graph.py
@@ -1,15 +1,18 @@
+import unittest
 import torch
 from tensorboardX import SummaryWriter
 
-dummy_input = (torch.zeros(1, 3),)
+class NumpyTest(unittest.TestCase):
+    def test_pytorch_graph(self):
+        dummy_input = (torch.zeros(1, 3),)
 
+        
+        class myLinear(torch.nn.Module):
+            def __init__(self):
+                super(myLinear, self).__init__()
+                self.l = torch.nn.Linear(3, 5)
+            def forward(self, x):
+                return self.l(x)
 
-class myLinear(torch.nn.Module):
-    def __init__(self):
-        super(myLinear, self).__init__()
-        self.l = torch.nn.Linear(3, 5)
-    def forward(self, x):
-        return self.l(x)
-
-with SummaryWriter(comment='LinearModel') as w:
-    w.add_graph(myLinear(), dummy_input, True)
+        with SummaryWriter(comment='LinearModel') as w:
+            w.add_graph(myLinear(), dummy_input, True)
