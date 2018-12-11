4f3e13859fc828a18382ca7f12dbb69a668b1b1a
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-12-11 22:32:25 +0800

    add_onnx_graph demo for mnist net

---
+++ examples/demo_onnx.py
@@ -0,0 +1,13 @@
+from tensorboardX import SummaryWriter
+
+import subprocess
+zoo_address = 'https://onnxzoo.blob.core.windows.net/models/opset_8/mnist/mnist.tar.gz'
+
+res = subprocess.call(['wget', '-nc', zoo_address])
+assert res == 0, 'cannot download example onnx model from the zoo'
+res = subprocess.call(['tar', 'xf', 'mnist.tar.gz', '-C', 'examples/', 'mnist/model.onnx'])
+
+
+
+with SummaryWriter() as w:
+    w.add_onnx_graph('examples/mnist/model.onnx')
