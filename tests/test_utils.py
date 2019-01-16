5135c856500f9e1902e469ea6317589ce90d2a43
David Lindner <dev@davidlindner.me>
2019-01-16 20:32:49 +0100

    fix reshaping in _prepare_video

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,10 +3,11 @@ from tensorboardX import summary
 import numpy as np
 import pytest
 import unittest
-from tensorboardX.utils import make_grid
+from tensorboardX.utils import make_grid, _prepare_video
 
 class UtilsTest(unittest.TestCase):
     def test_to_HWC(self):
+        np.random.seed(1)
         test_image = np.random.randint(0, 256, size=(3, 32, 32), dtype=np.uint8)
         converted = convert_to_HWC(test_image, 'chw')
         assert converted.shape == (32, 32, 3)
@@ -16,6 +17,16 @@ class UtilsTest(unittest.TestCase):
         test_image = np.random.randint(0, 256, size=(32, 32), dtype=np.uint8)
         converted = convert_to_HWC(test_image, 'hw')
         assert converted.shape == (32, 32, 3)
+
+    def test_prepare_video(self):
+        # at each timestep the sum over all other dimensions of the video should stay the same
+        np.random.seed(1)
+        V_before = np.random.random((4, 10, 3, 20, 20))
+        V_after = _prepare_video(np.copy(V_before))
+        V_before = np.swapaxes(V_before, 0, 1)
+        V_before = np.reshape(V_before, newshape=(10,-1))
+        V_after = np.reshape(V_after, newshape=(10,-1))
+        np.testing.assert_array_almost_equal(np.sum(V_before, axis=1), np.sum(V_after, axis=1))
 
 def convert_to_HWC(tensor, input_format):  # tensor: numpy array
     assert(len(set(input_format)) == len(input_format)), "You can not use the same dimension shordhand twice."
