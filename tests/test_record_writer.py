9c39916dc32b87d0bd03fc1ba8ec3941f964407c
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-01-22 00:02:33 +0800

    hello moto

--- tests/test_record_writer.py
+++ tests/test_record_writer.py
@@ -1,14 +1,25 @@
 from tensorboardX import SummaryWriter
 import unittest
-
 from tensorboardX.record_writer import S3RecordWriter, make_valid_tf_name
-class RecordWriterTest(unittest.TestCase):
-    def test_record_writer_s3(self):
-        writer = S3RecordWriter('s3://this/is/apen')
+import os
+import boto3
+from moto import mock_s3
 
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "foobar_key")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "foobar_secret")
+
+
+class RecordWriterTest(unittest.TestCase):
+    @mock_s3
+    def test_record_writer_s3(self):
+        client = boto3.client('s3', region_name='us-east-1')
+        client.create_bucket(Bucket='this')
+        writer = S3RecordWriter('s3://this/is/apen')
         bucket, path = writer.bucket_and_path()
         assert bucket == 'this'
         assert path == 'is/apen'
+        writer.write(bytes(42))
+        writer.flush()
 
     def test_make_valid_tf_name(self):
         newname = make_valid_tf_name('$ave/&sound')
