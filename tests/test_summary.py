27acb26bae061d1fed26e8e55c7ed97ed2f06ae3
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-03-09 15:06:07 +0800

    Do exact tests (#379)
    * add expect for test
    
    * fix histogram test

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -1,9 +1,10 @@
 from tensorboardX import summary
-
+from .expect_reader import compare_proto
 import numpy as np
 import pytest
 import unittest
-
+np.random.seed(0)
+# compare_proto = write_proto  # massive update expect
 
 class SummaryTest(unittest.TestCase):
     def test_uint8_image(self):
@@ -33,30 +34,50 @@ class SummaryTest(unittest.TestCase):
             summary.histogram('dummy', np.ndarray(0), 'tensorflow')
 
     def test_image_with_boxes(self):
-        summary.image_boxes('dummy',
+        compare_proto(summary.image_boxes('dummy',
                             np.random.rand(3, 32, 32).astype(np.float32),
-                            np.array([[10, 10, 40, 40]]))
+                            np.array([[10, 10, 40, 40]])), self)
 
     def test_image_with_one_channel(self):
-        summary.image('dummy', np.random.rand(1, 32, 32).astype(np.float32), dataformats='CHW')
-        summary.image('dummy', np.random.rand(4, 1, 32, 32).astype(np.float32), dataformats='NCHW')
-        summary.image('dummy', np.random.rand(4, 3, 32, 32).astype(np.float32), dataformats='NCHW')
-        summary.image('dummy', np.random.rand(32, 32).astype(np.float32), dataformats='HW')
+        np.random.seed(0)
+        compare_proto(summary.image('dummy', np.random.rand(1, 8, 8).astype(np.float32), dataformats='CHW'), self)
+
+    def test_image_with_one_channel_batched(self):
+        np.random.seed(0)
+        compare_proto(summary.image('dummy', np.random.rand(2, 1, 8, 8).astype(np.float32), dataformats='NCHW'), self)
+
+    def test_image_with_3_channel_batched(self):
+        np.random.seed(0)
+        compare_proto(summary.image('dummy', np.random.rand(2, 3, 8, 8).astype(np.float32), dataformats='NCHW'), self)
+
+    def test_image_without_channel(self):
+        np.random.seed(0)
+        compare_proto(summary.image('dummy', np.random.rand(8, 8).astype(np.float32), dataformats='HW'), self)
 
     def test_video(self):
         try:
             import moviepy
         except ImportError:
             return
+        np.random.seed(0)
+        compare_proto(summary.video('dummy', np.random.rand(4, 3, 1, 8, 8).astype(np.float32)), self)
         summary.video('dummy', np.random.rand(16, 48, 1, 28, 28).astype(np.float32))
-
+        #summary.video('dummy', np.random.rand(20, 7, 1, 8, 8).astype(np.float32))
     def test_audio(self):
-        summary.audio('dummy', np.random.rand(65535))
+        np.random.seed(0)
+        compare_proto(summary.audio('dummy', np.random.rand(42)), self)
 
     def test_text(self):
-        summary.text('dummy', 'text 123')
+        compare_proto(summary.text('dummy', 'text 123'), self)
 
-    def test_histogram(self):
-        summary.histogram('dummy', np.random.rand(1024), bins='auto', max_bins=5)
-        summary.histogram('dummy', np.random.rand(1024), bins='fd', max_bins=5)
-        summary.histogram('dummy', np.random.rand(1024), bins='doane', max_bins=5)
+    def test_histogram_auto(self):
+        np.random.seed(0)
+        compare_proto(summary.histogram('dummy', np.random.rand(1024), bins='auto', max_bins=5), self)
+
+    def test_histogram_fd(self):
+        np.random.seed(0)
+        compare_proto(summary.histogram('dummy', np.random.rand(1024), bins='fd', max_bins=5), self)
+
+    def test_histogram_doane(self):
+        np.random.seed(0)
+        compare_proto(summary.histogram('dummy', np.random.rand(1024), bins='doane', max_bins=5), self)
