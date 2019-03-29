24ae52fa807bc7561fce89d86bd170b080c68d7a
Nataliia Lunova <natalial@fb.com>
2019-03-29 11:13:08 -0700

    Fix unit tests

--- tests/test_pytorch_graph.py
+++ tests/test_pytorch_graph.py
@@ -1,25 +1,27 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 import unittest
 import torch
 from tensorboardX import SummaryWriter
+
 
 class PytorchGraphTest(unittest.TestCase):
     def test_pytorch_graph(self):
         dummy_input = (torch.zeros(1, 3),)
 
-        
         class myLinear(torch.nn.Module):
             def __init__(self):
                 super(myLinear, self).__init__()
-                self.l = torch.nn.Linear(3, 5)
+                self.linear = torch.nn.Linear(3, 5)
+
             def forward(self, x):
-                return self.l(x)
+                return self.linear(x)
 
         with SummaryWriter(comment='LinearModel') as w:
             w.add_graph(myLinear(), dummy_input, True)
 
     def test_wrong_input_size(self):
         print('expect error here:')
-        with self.assertRaises(RuntimeError) as e_info:
+        with self.assertRaises(TypeError):
             dummy_input = torch.rand(1, 9)
             model = torch.nn.Linear(3, 5)
             with SummaryWriter(comment='expect_error') as w:
