16041a6117c6e4b360fe88258c4fc8f55f53669b
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-06-08 13:13:18 +0800

    Make tests deterministic (#443)
    * replace np.random with predictable function.
    
    * update expect files
    
    * update .travis.yml to solve protobuf issue

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -1,19 +1,22 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from tensorboardX import summary
-from .expect_reader import compare_proto
+from .expect_reader import compare_proto, write_proto
 import numpy as np
 import pytest
 import unittest
-np.random.seed(0)
 # compare_proto = write_proto  # massive update expect
 
+def tensor_N(shape, dtype=float):
+    numel = np.prod(shape)
+    x = (np.arange(numel, dtype=dtype)).reshape(shape)
+    return x
 
 class SummaryTest(unittest.TestCase):
     def test_uint8_image(self):
         '''
         Tests that uint8 image (pixel values in [0, 255]) is not changed
         '''
-        test_image = np.random.randint(0, 256, size=(3, 32, 32), dtype=np.uint8)
+        test_image = tensor_N(shape=(3, 32, 32), dtype=np.uint8)
         scale_factor = summary._calc_scale_factor(test_image)
         assert scale_factor == 1, 'Values are already in [0, 255], scale factor should be 1'
 
@@ -22,7 +25,7 @@ class SummaryTest(unittest.TestCase):
         Tests that float32 image (pixel values in [0, 1]) are scaled correctly
         to [0, 255]
         '''
-        test_image = np.random.rand(3, 32, 32).astype(np.float32)
+        test_image = tensor_N(shape=(3, 32, 32))
         scale_factor = summary._calc_scale_factor(test_image)
         assert scale_factor == 255, 'Values are in [0, 1], scale factor should be 255'
 
@@ -37,50 +40,41 @@ class SummaryTest(unittest.TestCase):
 
     def test_image_with_boxes(self):
         compare_proto(summary.image_boxes('dummy',
-                            np.random.rand(3, 32, 32).astype(np.float32),
+                            tensor_N(shape=(3, 32, 32)),
                             np.array([[10, 10, 40, 40]])), self)
 
     def test_image_with_one_channel(self):
-        np.random.seed(0)
-        compare_proto(summary.image('dummy', np.random.rand(1, 8, 8).astype(np.float32), dataformats='CHW'), self)
+        compare_proto(summary.image('dummy', tensor_N(shape=(1, 8, 8)), dataformats='CHW'), self)
 
     def test_image_with_one_channel_batched(self):
-        np.random.seed(0)
-        compare_proto(summary.image('dummy', np.random.rand(2, 1, 8, 8).astype(np.float32), dataformats='NCHW'), self)
+        compare_proto(summary.image('dummy', tensor_N(shape=(2, 1, 8, 8)), dataformats='NCHW'), self)
 
     def test_image_with_3_channel_batched(self):
-        np.random.seed(0)
-        compare_proto(summary.image('dummy', np.random.rand(2, 3, 8, 8).astype(np.float32), dataformats='NCHW'), self)
+        compare_proto(summary.image('dummy', tensor_N(shape=(2, 3, 8, 8)), dataformats='NCHW'), self)
 
     def test_image_without_channel(self):
-        np.random.seed(0)
-        compare_proto(summary.image('dummy', np.random.rand(8, 8).astype(np.float32), dataformats='HW'), self)
+        compare_proto(summary.image('dummy', tensor_N(shape=(8, 8)), dataformats='HW'), self)
 
     def test_video(self):
         try:
             import moviepy
         except ImportError:
             return
-        np.random.seed(0)
-        compare_proto(summary.video('dummy', np.random.rand(4, 3, 1, 8, 8).astype(np.float32)), self)
-        summary.video('dummy', np.random.rand(16, 48, 1, 28, 28).astype(np.float32))
-        summary.video('dummy', np.random.rand(20, 7, 1, 8, 8).astype(np.float32))
+        compare_proto(summary.video('dummy', tensor_N(shape=(4, 3, 1, 8, 8))), self)
+        summary.video('dummy', tensor_N(shape=(16, 48, 1, 28, 28)))
+        summary.video('dummy', tensor_N(shape=(20, 7, 1, 8, 8)))
 
     def test_audio(self):
-        np.random.seed(0)
-        compare_proto(summary.audio('dummy', np.random.rand(42)), self)
+        compare_proto(summary.audio('dummy', tensor_N(shape=(42,))), self)
 
     def test_text(self):
         compare_proto(summary.text('dummy', 'text 123'), self)
 
     def test_histogram_auto(self):
-        np.random.seed(0)
-        compare_proto(summary.histogram('dummy', np.random.rand(1024), bins='auto', max_bins=5), self)
+        compare_proto(summary.histogram('dummy', tensor_N(shape=(1024,)), bins='auto', max_bins=5), self)
 
     def test_histogram_fd(self):
-        np.random.seed(0)
-        compare_proto(summary.histogram('dummy', np.random.rand(1024), bins='fd', max_bins=5), self)
+        compare_proto(summary.histogram('dummy', tensor_N(shape=(1024,)), bins='fd', max_bins=5), self)
 
     def test_histogram_doane(self):
-        np.random.seed(0)
-        compare_proto(summary.histogram('dummy', np.random.rand(1024), bins='doane', max_bins=5), self)
+        compare_proto(summary.histogram('dummy', tensor_N(shape=(1024,)), bins='doane', max_bins=5), self)
