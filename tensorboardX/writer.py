da9e51c8fbb0015bb0ad56af0b79baddebccef14
clefourrier <22726840+clefourrier@users.noreply.github.com>
2019-09-12 10:12:32 +0200

    Select the session group name in hparam
    This modification allows the user to select the haparam session group name he wants, instead of just using the date as a session id. 
    The display of hparams then gets way clearer.

--- tensorboardX/writer.py
+++ tensorboardX/writer.py
@@ -322,7 +322,7 @@ class SummaryWriter(object):
             self.all_writers = {self.file_writer.get_logdir(): self.file_writer}
         return self.file_writer
 
-    def add_hparams(self, hparam_dict=None, metric_dict=None):
+    def add_hparams(self, hparam_dict=None, metric_dict=None, name=str(time.time()))):
         """Add a set of hyperparameters to be compared in tensorboard.
 
         Args:
@@ -333,6 +333,7 @@ class SummaryWriter(object):
               here should be unique in the tensorboard record. Otherwise the value
               you added by `add_scalar` will be displayed in hparam plugin. In most
               cases, this is unwanted.
+            name (string): Personnalised name of the hparam session
 
         Examples::
 
@@ -351,7 +352,7 @@ class SummaryWriter(object):
             raise TypeError('hparam_dict and metric_dict should be dictionary.')
         exp, ssi, sei = hparams(hparam_dict, metric_dict)
 
-        with SummaryWriter(logdir=os.path.join(self.file_writer.get_logdir(), str(time.time()))) as w_hp:
+        with SummaryWriter(logdir=os.path.join(self.file_writer.get_logdir(), name) as w_hp:
             w_hp.file_writer.add_summary(exp)
             w_hp.file_writer.add_summary(ssi)
             w_hp.file_writer.add_summary(sei)
