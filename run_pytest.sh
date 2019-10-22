1db2de9302557a47f7865b1f7559e135ba353940
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-10-22 19:34:11 +0800

    add some documents and minor fixes

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,4 +1,10 @@
 pip install pytest boto3 moto onnx tensorboard matplotlib 
 
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python pytest
+if [ `ps|grep visdom |wc -l` = "1" ]
+    then
+    echo `ps|grep visdom |wc -l`
+    echo "no visdom"
+    visdom &
+fi
 
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python pytest
