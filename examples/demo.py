1fe65edab2a5aa72bb319338813f6f06382aba59
Tzu-Wei Huang <huang.dexter@gmail.com>
2021-06-05 23:25:55 +0800

    Revert unnecessary changes

--- examples/demo.py
+++ examples/demo.py
@@ -1,9 +1,9 @@
-from tensorboardX import SummaryWriter
 import torch
 import torchvision.utils as vutils
 import numpy as np
 import torchvision.models as models
 from torchvision import datasets
+from tensorboardX import SummaryWriter
 import datetime
 
 try:
