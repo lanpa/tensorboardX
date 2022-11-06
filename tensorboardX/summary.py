1040c55d39a4b58eceb4e59963a6a499bb60070d
Tzu-Wei Huang <huang.dexter@gmail.com>
2022-11-06 18:00:46 +0800

    fix PIL deprecation warning

--- tensorboardX/summary.py
+++ tensorboardX/summary.py
@@ -6,6 +6,7 @@ import logging
 import numpy as np
 import os
 import re as _re
+import packaging
 
 # pylint: disable=unused-import
 
@@ -320,6 +321,7 @@ def draw_boxes(disp_image, boxes, labels=None):
 
 def make_image(tensor, rescale=1, rois=None, labels=None):
     """Convert an numpy representation image to Image protobuf"""
+    import PIL
     from PIL import Image
     height, width, channel = tensor.shape
     scaled_height = int(height * rescale)
@@ -327,7 +329,11 @@ def make_image(tensor, rescale=1, rois=None, labels=None):
     image = Image.fromarray(tensor)
     if rois is not None:
         image = draw_boxes(image, rois, labels=labels)
-    image = image.resize((scaled_width, scaled_height), Image.ANTIALIAS)
+    if packaging.version.parse(PIL.__version__) >= packaging.version.parse('9.1.0'):
+        image = image.resize((scaled_width, scaled_height), Image.Resampling.LANCZOS)
+    else:
+        image = image.resize((scaled_width, scaled_height), Image.LANCZOS)
+
     import io
     output = io.BytesIO()
     image.save(output, format='PNG')
