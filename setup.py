d167c854b8f4f4a65be7285d47ad7bdee70b3070
Tzu-Wei Huang <huang.dexter@gmail.com>
2023-02-12 17:51:47 +0800

    Build and test python wheel with github actions (#687)
    * try to build with github actions

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 preparing_PyPI_package = 'sdist' in sys.argv or 'bdist_wheel' in sys.argv
-version_git = version = '2.5.1'
+version_git = version = subprocess.check_output(['git', 'describe', '--always']).decode('ascii').strip()
 
 if not preparing_PyPI_package:
     if os.path.exists('.git'):
@@ -43,6 +43,7 @@ with open('tensorboardX/__init__.py', 'a') as f:
 
 requirements = [
     'numpy',
+    'packaging',
     'protobuf>=3.8.0,<4',
 ]
 
