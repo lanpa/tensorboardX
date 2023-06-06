6488a222c2a9a929d2089a0c48cabbb89fad2e2b
Tzu-Wei Huang <huang.dexter@gmail.com>
2023-06-07 01:27:47 +0800

    Remove upper pins on protobuf (#697)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ preparing_PyPI_package = 'sdist' in sys.argv or 'bdist_wheel' in sys.argv
 requirements = [
     'numpy',
     'packaging',
-    'protobuf>=4.22.3,<5',
+    'protobuf>=4.22.3',
 ]
 
 
