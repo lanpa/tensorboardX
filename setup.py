e1658af1f202fd6ea23e5a582412497817edd503
Tzu-Wei Huang <huang.dexter@gmail.com>
2023-06-18 20:33:42 +0800

    Releases/2.6 (#699)
    * update history
    
    * launch
    
    * update tested python versions

--- setup.py
+++ setup.py
@@ -60,9 +60,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     cmdclass={
         'develop': PostDevelopCommand,
